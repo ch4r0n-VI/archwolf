f8fda40e6b8b9ddf88a7bd4fb82fa95602d67c4f
ch4r0n-VI <ch4r0n_VI@protonmail.com>
2021-10-24 17:49:32 +0200

    Updated releng profile definition

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="archwolf"
+iso_label="ARCHWOLF_$(date +%Y%m)"
+iso_publisher="ArchWolf <https://github.com/ch4r0n-VI>"
+iso_application="ArchWolf Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
